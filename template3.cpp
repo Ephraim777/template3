f9570ef9d6a3a38eb7bbf3f34a26c9e58c07dd1d
Ephraim777 <165553036+Ephraim777@users.noreply.github.com>
2024-08-19 13:55:49 +0300

    Update template3.cpp

--- template3.cpp
+++ template3.cpp
@@ -1,116 +1,114 @@
 #include<iostream>
 using namespace std;
+
 template<typename T>
 class MyClassVector
 {
-  T*m_array;
+    T* m_array;
+    int m_capacity;
+    int m_size;
 
-  int m_capacity;
+    using reference = T&;
 
-  int m_size;
+public:
+    MyClassVector(T num, int count) : m_capacity(count), m_size(count)
+    {
+        m_array = new T[count];
+        for (int i = 0; i < count; i++)
+        {
+            m_array[i] = num;
+        }
+    }
 
-  using reference = T&;
+    MyClassVector() : m_capacity(0), m_size(0), m_array(nullptr) {}
 
-  public:
+    ~MyClassVector()
+    {
+        delete[] m_array;
+        m_array = nullptr;
+    }
 
-MyClassVector(T num, int count): m_capacity(count),m_size(count)
-{
-  m_array = new T[count];
+    int Size() const
+    {
+        return m_size;
+    }
 
-  for(int i =0; i<count; i++)
-{ 
-    m_array[i] = num;
-}
-}
+    int Capacity() const
+    {
+        return m_capacity;
+    }
 
-MyClassVector(): m_capacity(0), m_size(0), m_array(nullptr){}
-~MyClassVector()
-{
-  delete [] m_array;
-  m_array = nullptr;
-}
+    void push_back(T value)
+    {
+        if (m_size == m_capacity)
+        {
+            if (!m_capacity)
+                m_capacity = 1;
+            
+            m_capacity = m_capacity * 2;
+            T* new_array = new T[m_capacity];
 
- int Size() const
- {
-   return m_size;
- }
+            if (m_array != nullptr)
+            {
+                for (int i = 0; i < m_size; i++)
+                {
+                    new_array[i] = m_array[i];
+                }
+                delete[] m_array;
+            }
 
- int Capacity() const
- {
-   return m_capacity;
- }
+            m_array = new_array;
+            new_array = nullptr;
+        }
+        
+        m_array[m_size] = value;
+        m_size++;
+    }
 
- void push_back( T value)
- {
-   if (m_size == m_capacity)
- {
-     if(!m_capacity)
+    reference at(int index)
+    {
+        if (index >= m_size)
+        {
+            throw std::out_of_range("error index");
+        }
 
-     m_capacity = 1;
- 
-     m_capacity = m_capacity*2;
+        return m_array[index];
+    }
 
-     T* new_array = new T[m_capacity];
-
-     if(m_array!= nullptr)
- {
-   for(int i = 0; i < m_size; i++)
- {
-     new_array[i]=m_array[i];
- }
-    delete [] m_array;
- }
-   m_array = new_array;
-   new_array = nullptr;
- }
-   m_array[m_size] = value;
-   m_size++;
- }
-
- reference at(int index)
-  {
-     if(index >= m_size)
-  {
-     throw std::out_of_range("error index");
-  }
-    return m_array[index];
-  }
-
- reference operator [] (int index)
-  {
-    return m_array[index];
-  }
+    reference operator[] (int index)
+    {
+        return m_array[index];
+    }
 };
+
 int main()
 {
     MyClassVector<int> v;
-    
+
     v.push_back(1);
-    
     std::cout << "Size: " << v.Size() << std::endl;
-    std::cout << "Capacity: "<< v.Capacity() << std::endl;
+    std::cout << "Capacity: " << v.Capacity() << std::endl;
+
     v.push_back(2);
-    
     std::cout << "Size: " << v.Size() << std::endl;
-    std::cout << "Capacity: "<< v.Capacity() << std::endl;
+    std::cout << "Capacity: " << v.Capacity() << std::endl;
+
     v.push_back(3);
-    
     std::cout << "Size: " << v.Size() << std::endl;
-    std::cout << "Capacity: "<< v.Capacity() << std::endl;
-    
+    std::cout << "Capacity: " << v.Capacity() << std::endl;
+
     for (int i = 0; i < v.Size(); i++)
     {
-      std:: cout << v[i] << " "; 
+        std::cout << v[i] << " ";
     }
-    
-    std::cout<< std::endl;
-    
+    std::cout << std::endl;
+
     v.at(1) = 7;
-    
+
     for (int i = 0; i < v.Size(); i++)
     {
-      std:: cout << v[i] << " "; 
+        std::cout << v[i] << " ";
     }
-    
+
     return 0;
 }
