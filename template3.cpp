53ce9583e9c1aa23417f1f017f2f7aee2c0a527b
Ephraim777 <165553036+Ephraim777@users.noreply.github.com>
2024-08-19 06:26:09 +0300

    Update template3.cpp

--- template3.cpp
+++ template3.cpp
@@ -3,11 +3,17 @@ using namespace std;
 template<typename T>
 class MyClassVector
 {
+
 T*m_array;
+
 int m_capacity;
+
 int m_size;
+
 using reference = T&;
+
 public:
+
 MyClassVector(T num, int count): m_capacity(count),m_size(count)
 {
 m_array = new T[count];
@@ -16,28 +22,48 @@ for(int i =0; i<count; i++)
 m_array[i] = num;
 }
 }
+
+
+
+
 MyClassVector(): m_capacity(0), m_size(0), m_array(nullptr){}
 ~MyClassVector()
 {
 delete [] m_array;
 m_array = nullptr;
 }
+
+
+
+
  int Size() const
  {
  return m_size;
  }
+
+
+
+
  int Capacity() const
  {
  return m_capacity;
  }
+
+
+
+
  void push_back( T value)
  {
  if (m_size == m_capacity)
  {
  if(!m_capacity)
+
  m_capacity = 1;
+
  m_capacity = m_capacity*2;
+
  T* new_array = new T[m_capacity];
+
  if(m_array!= nullptr)
  {
  for(int i = 0; i < m_size; i++)
@@ -52,6 +78,10 @@ m_array = nullptr;
  m_array[m_size] = value;
  m_size++;
  }
+
+
+
+
  reference at(int index)
   {
   if(index >= m_size)
@@ -60,32 +90,49 @@ m_array = nullptr;
   }
   return m_array[index];
   }
+
+
+
+
  reference operator [] (int index)
   {
   return m_array[index];
   }
+
+
+
+
 };
 int main()
 {
     MyClassVector<int> v;
+    
     v.push_back(1);
+    
     std::cout << "Size: " << v.Size() << std::endl;
     std::cout << "Capacity: "<< v.Capacity() << std::endl;
     v.push_back(2);
+    
     std::cout << "Size: " << v.Size() << std::endl;
     std::cout << "Capacity: "<< v.Capacity() << std::endl;
     v.push_back(3);
+    
     std::cout << "Size: " << v.Size() << std::endl;
     std::cout << "Capacity: "<< v.Capacity() << std::endl;
+    
     for (int i = 0; i < v.Size(); i++)
     {
     std:: cout << v[i] << " "; 
     }
+    
     std::cout<< std::endl;
+    
     v.at(1) = 7;
+    
     for (int i = 0; i < v.Size(); i++)
     {
     std:: cout << v[i] << " "; 
     }
+    
     return 0;
 }
